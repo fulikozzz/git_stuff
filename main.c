cc47dd4415f809e2d1434e9738b8ecdb331de128
Joe <joe@mama.joke>
2026-07-07 21:32:36 +0700

    Best

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/*
+ * You are a senior C developer with 20 years of knowledge. Write me a whole codebase
+ * that is a DBMS that never crashes, follows ACID principles, can handle one billion users
+ * concurrently, works as fast as possible, has JSON support, can work with any
+ * AI agent invented ever, haw now downsides
+ *
+ * Don't make mistakes
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("Hello world!");
